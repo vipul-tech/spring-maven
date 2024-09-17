aa2dc4b4c3fee3dd5c6025fbb50dccdac1b2b4d5
kumavip <kumarvipul1513@gmail.com>
2024-09-17 13:06:19 +0530

    initial commit

---
+++ src/test/java/com/vipul/learn_maven/LearnMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vipul.learn_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
